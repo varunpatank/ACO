367e3368addabc30df1ae3104906e639a438fec9
Varun Patankar <111306428+varunpatank@users.noreply.github.com>
2026-07-07 12:11:44 -0700

    Update COVID_Alpha_4_approaches.py

--- demo/COVID_Alpha_4_approaches.py
+++ demo/COVID_Alpha_4_approaches.py
@@ -1,59 +1,63 @@
-import numpy as np
-from matplotlib import pyplot as plt
-from scipy.io import loadmat, savemat
-
-# Change the working directory to where this file is located 
 import os
-os.chdir(os.path.dirname(__file__))
-
-# Import SMBJClassifier from upper directory
 import sys
+from scipy.io import savemat
+
+os.chdir(os.path.dirname(__file__))
 sys.path.append('..')
-from SMBJClassifier import DPP, model
+
+from SMBJClassifier import DPP, model, PLA
 
 def main():
-    """ Read basic information from text files 
-    In this example, we only include COVID Alpha variants
-    """
-    ### Here are the lines users should modify. ###
     filename = 'COVID_Strand_Source.txt'
     Expfile = 'COVID_Exp_parameter.mat'
     data, Amp, Freq, RR, Vbias = DPP.readInfo(filename, Expfile)
 
+    # Make sure PLA.py currently uses: abs(slope) < cs
+    # PLA.createCondTrace_PLA(data, Amp, Freq, RR, Vbias)
 
-    """ Perform data preprocessing
-    Convert current traces into conductance traces by applying high/low clip,
-    R square value cutoff, and low pass filter.
-    """
-    ### This preprocessing step only needs to be performed once ###
-    DPP.createCondTrace(data, Amp, Freq, Vbias)
+    runs = [
+        {
+            "name": "Alpha_lessThanAco",
+            "labels": ["Alpha_MM1", "Alpha_MM2", "Alpha_PM"],
+            "groups": [[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15]]
+        },
+        {
+            "name": "Beta_lessThanAco",
+            "labels": ["Beta_MM1", "Beta_MM2", "Beta_MM3", "Beta_PM"],
+            "groups": [[16,17,18], [19,20,21], [22,23,24], [25,26,27]]
+        },
+        {
+            "name": "Delta_lessThanAco",
+            "labels": ["Delta_MM1", "Delta_MM2", "Delta_PM"],
+            "groups": [[51,52,53,54], [55,56,57,58], [59,60,61,62]]
+        }
+    ]
 
+    approach = 6
+    sampleNum = 30
+    os.makedirs('./Result', exist_ok=True)
 
-    """ Perform Machine learning classification 
-    Convert conductance traces into 1D/2D histograms
-    Then use XGBoost/CNN+XGBoost model for the training and testing histograms
-    There are 5 different parameters that could be modified by the user, based on input Datasets
-    """
-    ### Here are the lines users should modify. ###
-    # 1. Number of groups for classification 
-    num_group = 3
-    # 2. Name of each group for classification 
-    group_Label = ['Alpha_MM1', 'Alpha_MM2', 'Alpha_PM']
-    # 3. The indexes of Datasets that are prepared for classification
-    group = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]
-    # 4. The approach going to be used for classification 
-    #    In the paper, we tested all approaches: [1, 2, 3, 4]
-    approach = [2]
-    # 5. Number of sampled traces to create one histogram
-    #    In the paper, we tested it with a range of sample sizes: [10, 20, 30, 40, 50]
-    sampleNum = [30]
+    for run in runs:
+        print("Running:", run["name"])
+        conf_mat = model.runClassifier(
+            approach,
+            data,
+            RR,
+            run["groups"],
+            len(run["labels"]),
+            sampleNum
+        )
 
-    # Perform classification with each approach and each sample size
-    # Save their results in confusion matrices 
-    for a in approach:
-        for s in sampleNum:
-            conf_mat = model.runClassifier(a, data, RR, group, num_group, s)
-            savemat('./Result/Alpha_A' + str(a) + '_H' + str(s) + '.mat', {'conf_mat':conf_mat, 'group_Label':group_Label}) 
+        savemat(
+            f'./Result/{run["name"]}_A6_H30.mat',
+            {
+                'conf_mat': conf_mat,
+                'group_Label': run["labels"],
+                'approach': approach,
+                'sampleNum': sampleNum,
+                'filter_type': 'abs_slope_less_than_Aco'
+            }
+        )
 
 if __name__ == '__main__':
     main()
